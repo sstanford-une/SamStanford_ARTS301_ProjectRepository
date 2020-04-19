5967fdf77d9c20a56cc0730115e1881cf3ace4e4
Sammy Stanford <1014268@student.sae.edu.au>
2020-04-19 17:39:56 +1000

    Update
    Triggers now activate and deactivate

---
+++ Assets/Scripts/ControlScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScript : MonoBehaviour
+{
+    
+    GameObject[] artObjects = new GameObject[6];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CollectArt()
+    {
+        int artCount = GameObject.FindGameObjectsWithTag("Artwork").Length;
+        for (int i = 0; i < artCount; i++)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/TriggerRadius.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerRadius : MonoBehaviour
+{
+    public GameObject artObject;
+    public bool active;
+    string artName;
+    string triggerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TriggerCapture();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TriggerCapture()
+    {
+        artName = artObject.name;
+        triggerName = (artName + "." + gameObject.name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        active = true;
+        Debug.Log(triggerName + " activated.");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        active = false;
+        Debug.Log(triggerName + " deactivated.");
+    }
+}
