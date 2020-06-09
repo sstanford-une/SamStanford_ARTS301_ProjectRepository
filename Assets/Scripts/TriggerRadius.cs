a28dd85061689005f35847cd0159447f504ee27b
Sammy Stanford <1014268@student.sae.edu.au>
2020-06-09 20:36:36 +1000

    Update

--- Assets/Scripts/TriggerRadius.cs
+++ Assets/Scripts/TriggerRadius.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TriggerRadius : MonoBehaviour
 {
     public GameObject artObject;
+    public FMOD.Studio.EventInstance Music, Room, Blue1, Blue2, Blue3, Blue4, Purple1, Purple2, Purple3, Purple4, Green1, Green2, Green3, Green4, Orange1, Orange2, Orange3, Orange4;
     public bool active;
     string artName;
     string triggerName;
@@ -13,6 +14,8 @@ public class TriggerRadius : MonoBehaviour
     void Start()
     {
         TriggerCapture();
+        SetValues();
+        StartPlayback();
     }
 
     // Update is called once per frame
@@ -37,5 +40,48 @@ public class TriggerRadius : MonoBehaviour
     {
         active = false;
         Debug.Log(triggerName + " deactivated.");
+    }
+
+    void SetValues()
+    {
+        Room = FMODUnity.RuntimeManager.CreateInstance("event:/Room");
+        Blue1 = FMODUnity.RuntimeManager.CreateInstance("event:/B1");
+        Blue2 = FMODUnity.RuntimeManager.CreateInstance("event:/B2");
+        Blue3 = FMODUnity.RuntimeManager.CreateInstance("event:/B3");
+        Blue4 = FMODUnity.RuntimeManager.CreateInstance("event:/B4");
+        Green1 = FMODUnity.RuntimeManager.CreateInstance("event:/G1");
+        Green2 = FMODUnity.RuntimeManager.CreateInstance("event:/G2");
+        Green3 = FMODUnity.RuntimeManager.CreateInstance("event:/G3");
+        Green4 = FMODUnity.RuntimeManager.CreateInstance("event:/G4");
+        Purple1 = FMODUnity.RuntimeManager.CreateInstance("event:/P1");
+        Purple2 = FMODUnity.RuntimeManager.CreateInstance("event:/P2");
+        Purple3 = FMODUnity.RuntimeManager.CreateInstance("event:/P3");
+        Purple4 = FMODUnity.RuntimeManager.CreateInstance("event:/P4");
+        Orange1 = FMODUnity.RuntimeManager.CreateInstance("event:/O1");
+        Orange2 = FMODUnity.RuntimeManager.CreateInstance("event:/O2");
+        Orange3 = FMODUnity.RuntimeManager.CreateInstance("event:/O3");
+        Orange4 = FMODUnity.RuntimeManager.CreateInstance("event:/O4");
+
+    }
+
+    void StartPlayback()
+    {
+        Room.start();
+        Blue1.start();
+        Blue2.start();
+        Blue3.start();
+        Blue4.start();
+        Green1.start();
+        Green2.start();
+        Green3.start();
+        Green4.start();
+        Purple1.start();
+        Purple2.start();
+        Purple3.start();
+        Purple4.start();
+        Orange1.start();
+        Orange2.start();
+        Orange3.start();
+        Orange4.start();
     }
 }
